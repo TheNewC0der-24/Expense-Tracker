71cbaee493e8c8439f4660f485a74dc119fb0686
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:41:31 +0530

    import grid from @MUI and Details component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Details from './components/Details/Details';
+
+import useStyles from './appStyles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <div>
-            App
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify='center' style={{ height: '100vh' }} >
+                <Grid item xs={12} sm={4} >
+                    <Details />
+                </Grid>
+                <Grid item xs={12} sm={4} >
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4} >
+                    <Details />
+                </Grid>
+            </Grid>
         </div>
     )
 }
