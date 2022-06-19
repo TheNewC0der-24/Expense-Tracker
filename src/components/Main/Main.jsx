a27237c4a52e23fb6d4de4a6a8d7b0d38257e8de
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:52:53 +0530

    Create Main component and import @MUI

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+const Main = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Main;
