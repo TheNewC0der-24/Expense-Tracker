cb0435e822e6f733a661b4364069d16354e1bf68
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:49:06 +0530

    Improve styling

--- src/components/Details/detailsStyles.js
+++ src/components/Details/detailsStyles.js
@@ -2,9 +2,9 @@ import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
     income: {
-        borderBottom: '10px solid rgba(0,255,0,0.5)'
+        borderBottom: '10px solid rgba(0, 255, 0, 1)'
     },
     expense: {
-        borderBottom: '10px solid rgba(255,0,0,0.5)'
+        borderBottom: '10px solid rgba(255, 0, 0, 1)'
     }
 }));
