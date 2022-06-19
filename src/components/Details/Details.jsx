8c86f3f902743029157293ce2b34472a9deae1ab
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:14:50 +0530

    Create Details component and import @MUI

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+const Details = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Details;
