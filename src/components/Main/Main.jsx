759cdae1aabdc93890bd68d90e02d5a2f2643f55
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 19:40:37 +0530

    Create Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Card, CardHeader, CardContent, Typography, Divider, Grid } from '@material-ui/core';
+
+import useStyles from './mainStyles';
 
 const Main = () => {
-    return (
-        <div>
+    const classes = useStyles();
 
-        </div>
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardContent>
+                <Typography align='center' variant="h5">
+                    Total Balance $100
+                </Typography>
+                <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+                    {/* InfoCard... */}
+                    Try saying : Add income or expense for $100 in Category Salary for Monday...
+                </Typography>
+                <Divider />
+                {/* Form */}
+            </CardContent>
+
+            <CardContent className={classes.cartContent}>
+                <Grid container spacing={2} >
+                    <Grid item xs={12}>
+                        {/* <List/> */}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
     )
 }
 
