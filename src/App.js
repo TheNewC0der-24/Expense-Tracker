9c639eaa856b054435cad0c025e4fbddc6f989cc
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:49:38 +0530

    Adding title to particular card

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ const App = () => {
         <div>
             <Grid className={classes.grid} container spacing={0} alignItems="center" justify='center' style={{ height: '100vh' }} >
                 <Grid item xs={12} sm={4} >
-                    <Details />
+                    <Details title='Income' />
                 </Grid>
                 <Grid item xs={12} sm={4} >
                     Main
                 </Grid>
                 <Grid item xs={12} sm={4} >
-                    <Details />
+                    <Details title='Expense' />
                 </Grid>
             </Grid>
         </div>
