46f07fcab44ae72f01ec3aeb6f4f539dc316b8bd
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-20 04:54:31 +0530

    Create delete dispatch function

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
 
+import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from './listStyles';
 
 const List = () => {
     const classes = useStyles();
-
-    const transactions = [
-        { id: 1, type: 'Income', category: 'Salary', amount: 50, date: "Wed Dec 16" },
-        { id: 2, type: 'Expense', category: 'Pets', amount: 50, date: "Wed Dec 17" },
-        { id: 3, type: 'Income', category: 'Business', amount: 150, date: "Wed Dec 18" }
-    ];
+    const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
 
     return (
         <MUIList dense={false} className={classes.list}>
@@ -25,7 +21,7 @@ const List = () => {
                         </ListItemAvatar>
                         <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
                         <ListItemSecondaryAction>
-                            <IconButton edge="end" aria-label="delete" onClick=''>
+                            <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
                                 <Delete />
                             </IconButton>
                         </ListItemSecondaryAction>
