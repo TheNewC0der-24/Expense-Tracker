1900d3faf763576f5abf811782df1a6883691107
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:18:47 +0530

    Import Doughnut from @reactchartjs

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
 
 const Details = () => {
     return (
-        <div>
-
-        </div>
+        <Card>
+            <CardHeader title='Income' />
+            <CardContent>
+                <Typography variant='h5'>
+                    $50
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
