8eb4a7f5465c6a77f61eabeedd963642dbcb9d06
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-20 04:04:05 +0530

    Create action creators

--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,18 @@
 import React, { useReducer, createContext } from "react";
 
+import contextReducer from "./contextReducer";
+
 const initialState = [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
+    const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+    // ACTION CREATORS
+    const deleteTransaction = (id) => dispatch({ type: "DELETE_TRANSACTION", payload: id });
+    const addTransaction = (transaction) => dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+
     return (
         <ExpenseTrackerContext.Provider value={{ appName: 'Expense Tracker' }}>
             {children}
