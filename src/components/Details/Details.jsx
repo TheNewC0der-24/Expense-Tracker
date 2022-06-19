744f572931719285e8bb0d00ed6bd7b6afca4933
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-19 18:40:16 +0530

    Create basic UI design for Detail card

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 
+import useStyles from './detailsStyles';
+
 const Details = () => {
+    const classes = useStyles();
+
     return (
-        <Card>
+        <Card className={classes.income}>
             <CardHeader title='Income' />
             <CardContent>
                 <Typography variant='h5'>
                     $50
                 </Typography>
+                {/* <Doughnut data="DATA" /> */}
             </CardContent>
         </Card>
     )
