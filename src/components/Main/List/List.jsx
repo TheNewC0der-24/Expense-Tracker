b84b8e749a212a03188faf05ef94775962fa8354
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-20 03:40:37 +0530

    Create List component

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+
+import useStyles from './listStyles';
+
+const List = () => {
+    const classes = useStyles();
+
+    const transactions = [
+        { id: 1, type: 'Income', category: 'Salary', amount: 50, date: "Wed Dec 16" },
+        { id: 2, type: 'Expense', category: 'Pets', amount: 50, date: "Wed Dec 17" },
+        { id: 3, type: 'Income', category: 'Business', amount: 150, date: "Wed Dec 18" }
+    ];
+
+    return (
+        <MUIList dense={false} className={classes.list}>
+            {transactions.map((transaction) => (
+                <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                                <MoneyOff />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="delete" onClick=''>
+                                <Delete />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                </Slide>
+            ))}
+        </MUIList>
+    )
+}
+
+export default List;
