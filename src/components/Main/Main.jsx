f60f840b713c62939a297b377b6ad19a66db9a6e
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-20 03:41:11 +0530

    import List component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import { Card, CardHeader, CardContent, Typography, Divider, Grid } from '@mater
 
 import useStyles from './mainStyles';
 import Form from './Form/Form';
+import List from './List/List';
 
 const Main = () => {
     const classes = useStyles();
@@ -25,7 +26,7 @@ const Main = () => {
             <CardContent className={classes.cartContent}>
                 <Grid container spacing={2} >
                     <Grid item xs={12}>
-                        {/* <List/> */}
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
