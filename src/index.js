b3696c24f5ded6c20477771d62f8cdbc536dcaf3
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-21 00:51:56 +0530

    import Speechly and provided it to the application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider appId='b77c216a-f270-47a9-978c-aaaa67a0dea9' language="en-US" >
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById('root')
 )
