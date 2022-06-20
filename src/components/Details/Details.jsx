1ec359efddbe8b1131711f35593ff170627fd968
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-20 20:25:49 +0530

    Populate the custom useTransactions hook to show total amount and chart data

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 
+import useTransactions from '../../useTransactions';
 import useStyles from './detailsStyles';
 
 const Details = ({ title }) => {
     const classes = useStyles();
+
+    const { total, chartData } = useTransactions(title);
 
     return (
         <Card className={title === 'Income' ? classes.income : classes.expense}>
             <CardHeader title={title} />
             <CardContent>
                 <Typography variant='h5'>
-                    $50
+                    ${total}
                 </Typography>
-                {/* <Doughnut data="DATA" /> */}
+                <Doughnut data={chartData} />
             </CardContent>
         </Card >
-    )
+    );
 }
 
 export default Details;
