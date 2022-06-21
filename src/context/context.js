426671f46707aa4704a6a16da7b6185d59d301c2
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-21 17:02:10 +0530

    Create context for balance

--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,10 @@ export const Provider = ({ children }) => {
     const deleteTransaction = (id) => dispatch({ type: "DELETE_TRANSACTION", payload: id });
     const addTransaction = (transaction) => dispatch({ type: "ADD_TRANSACTION", payload: transaction });
 
+    const balance = transactions.reduce((acc, currVal) => currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount, 0);
+
     return (
-        <ExpenseTrackerContext.Provider value={{ deleteTransaction, addTransaction, transactions }}>
+        <ExpenseTrackerContext.Provider value={{ deleteTransaction, addTransaction, transactions, balance }}>
             {children}
         </ExpenseTrackerContext.Provider>
     )
