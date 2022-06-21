f1380b23e00463b20cd7d97a15971d71af9ee28f
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-21 18:51:43 +0530

    Improve UI design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -23,7 +23,7 @@ const Main = () => {
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                     <InfoCard />
                 </Typography>
-                <Divider />
+                <Divider className={classes.divider} />
                 <Form />
             </CardContent>
 
