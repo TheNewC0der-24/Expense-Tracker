73cd704e2ed770a49302b368187b3b8177291540
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-21 14:37:11 +0530

    Add functionality for automatically creating the transactions

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -67,6 +67,10 @@ const Form = () => {
                         break;
                 }
             });
+
+            if (segment.isFinal && formData.amount && formData.category && formData.type && formData.date) {
+                createTransaction();
+            }
         }
     }, [segment]);
 
