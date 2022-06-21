accd084f0df00463064be3f0e29665246bcefaf3
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-21 18:42:10 +0530

    Improve responsiveness for mobile devices

--- src/App.js
+++ src/App.js
@@ -12,14 +12,19 @@ const App = () => {
     return (
         <div>
             <Grid className={classes.grid} container spacing={0} alignItems="center" justifyContent='center' style={{ height: '100vh' }} >
-                <Grid item xs={12} sm={4} >
+                <Grid item xs={12} sm={4} className={classes.mobile} >
                     <Details title='Income' />
                 </Grid>
-                <Grid item xs={12} sm={3} >
+
+                <Grid item xs={12} sm={3} className={classes.main} >
                     <Main />
                 </Grid>
+                <Grid item xs={12} sm={4} className={classes.desktop} >
+                    <Details title='Income' />
+                </Grid>
+
                 <Grid item xs={12} sm={4} >
-                    <Details title='Expense' />
+                    <Details title='Expense' className={classes.last} />
                 </Grid>
             </Grid>
             <PushToTalkButtonContainer>
