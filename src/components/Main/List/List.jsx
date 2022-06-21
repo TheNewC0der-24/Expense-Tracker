10ab8d917e110ed1c2308de541ac2118b9943b90
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-06-21 14:35:52 +0530

    Change currency logo to indian rupee and add color style to delete icon

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
-import { Delete, MoneyOff } from '@material-ui/icons';
+import { Delete } from '@material-ui/icons';
 
 import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from './listStyles';
@@ -16,13 +16,13 @@ const List = () => {
                     <ListItem>
                         <ListItemAvatar>
                             <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
-                                <MoneyOff />
+                                <i className='bx bx-rupee' />
                             </Avatar>
                         </ListItemAvatar>
-                        <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+                        <ListItemText primary={transaction.category} secondary={`Rs.${transaction.amount} - ${transaction.date}`} />
                         <ListItemSecondaryAction>
                             <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
-                                <Delete />
+                                <Delete style={{ color: '#ff0000' }} />
                             </IconButton>
                         </ListItemSecondaryAction>
                     </ListItem>
